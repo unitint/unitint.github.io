6b94bb082b8102fa5040475e618f82f262bbc33e
unitint <unitint92@gmail.com>
2025-01-09 13:20:26 +0800

    add hero section

--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
-// Hide loading screen and show main content after 3 seconds
-window.onload = function () {
-    setTimeout(function () {
-        document.getElementById('loading-screen').style.opacity = '0'; // Fade out
-        setTimeout(function () {
-            document.getElementById('loading-screen').style.display = 'none'; // Hide it
-            document.body.style.display = 'block'; // Show main content
-        }, 500); // Wait for fade-out
-    }, 3000); // 3 seconds delay
-};
 
 // Toggle navbar visibility on mobile
 const navbarToggle = document.querySelector('.navbar-toggle');
